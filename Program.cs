c6ef23a0a555a2fa06d41997234a5ed00d49d6b9
Igor Daniel <igordaniel1903@gmail.com>
2024-01-10 17:34:46 -0300

    Desafio concluídogit push!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Teste do Nokia:");
+Nokia nokiacell = new Nokia("10291203","model-1", "000099999", 64);
+nokiacell.Ligar();
+nokiacell.InstalarAplicativo("Tik tok");
+
+Console.WriteLine("Teste do Iphone: ");
+Iphone iphonecell = new Iphone("12340512","model-2","0000001111",124);
+iphonecell.Ligar();
+iphonecell.InstalarAplicativo("Instagram");
